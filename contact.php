badff53a9c68c184cca6b764d7791bce7c783272
Lorelei <lhoover87@gmail.com>
2015-04-13 16:54:22 -0400

    contact.php
    A simple contact form

---
+++ contact.php
@@ -0,0 +1,17 @@
+<head>
+<title>Contact Form</title>
+
+<body>
+<h1>Contact Us</h1>
+<form action="thankyou.php" method="post">
+	Name: <input type="text" name="fullname"><br />
+
+	Email: <input type="text" name="email"><br />
+
+	Message:
+<textarea name="message" cols="30" rows="10"></textarea>
+	<input type="submit" value="Submit">
+
+</form>
+
+</head>
